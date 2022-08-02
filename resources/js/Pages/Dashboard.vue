4427940182b912bb97ad8bdf5e2038813f4e6fce
Luna Muylkens <93606228+LunashaGit@users.noreply.github.com>
2022-08-02 03:29:49 +0200

    Add Images profile dashboard

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,6 +1,16 @@
-<script setup>
+<script>
 import BreezeAuthenticatedLayout from '@/Layouts/Authenticated.vue';
-import { Head } from '@inertiajs/inertia-vue3';
+import {Head, Link} from '@inertiajs/inertia-vue3';
+export default {
+    components: {
+        Head,
+        Link,
+        BreezeAuthenticatedLayout,
+    },
+    props: {
+        auth: Object,
+    },
+};
 </script>
 
 <template>
@@ -9,15 +19,15 @@ import { Head } from '@inertiajs/inertia-vue3';
     <BreezeAuthenticatedLayout>
         <template #header>
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Dashboard
+                Dashboard Profile
             </h2>
         </template>
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 bg-white border-b border-gray-200">
-                        You're logged in!
+                    <div class="p-6 bg-white border-b border-gray-200 flex justify-center align-middle">
+                        <img class="h-1/6 w-1/6 rounded-full" v-bind:src="('storage/images/' + auth.user.profile_picture)" />
                     </div>
                 </div>
             </div>
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
 use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
