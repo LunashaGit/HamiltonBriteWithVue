d38ef9f39bb9151b72f0b6c0fbc0902990d57eee
LunashaGit <lunamuylkens@gmail.com>
2022-08-03 11:41:33 +0200

    Card Design Post add

--- resources/js/Pages/Post/Index.vue
+++ resources/js/Pages/Post/Index.vue
@@ -29,39 +29,23 @@ export default {
             <div class="container mx-auto sm:px-6 lg:px-8">
                 <div
                     class="
-                        overflow-hidden
-                        bg-white
-                        shadow-sm
-                        sm:rounded-lg
+                        flex
+                        flex-row
+                        justify-center
+                        items-center
                     "
                 >
-                    <div class="p-6 bg-white border-b border-gray-200">
-                        <table class="w-full">
-                            <thead class="font-bold bg-gray-300 border-b-2">
-                            <td class="px-4 py-2">ID</td>
-                            <td class="px-4 py-2">Title</td>
-                            <td class="px-4 py-2">Description</td>
-                            <td class="px-4 py-2">Action</td>
-                            </thead>
-                            <tbody>
-                            <tr v-for="post in posts.data" :key="post.id">
-                                <td class="px-4 py-2">
-                                    {{ post.id }}
-                                </td>
-                                <td class="px-4 py-2">
-                                    {{ post.title }}
-                                </td>
-                                <td class="px-4 py-2">
-                                    {{ post.body }}
-                                </td>
-                                <td class="px-4 py-2">
-                                    <Link :href="route('post.index', { slug: post.slug})">Show More</Link>
-                                </td>
-                            </tr>
-                            </tbody>
-                        </table>
-                        <pagination class="mt-6" :links="posts.links" />
+                    <div v-for="post in posts.data" :key="post.id" class=" w-96 h-60 my-12 gap-5  bg-white border-b border-gray-200 flex flex-col justify-between items-center mx-12">
+                        <img class="h-36 w-36 -mt-10 lg:-mt-16 shadow-2xl" v-bind:src="('storage/images/' + post.image)" />
+                        <h2 class="text-center">
+                            {{ post.title }}
+                        </h2>
+                        <p class="text-center">
+                            {{ post.body }}
+                        </p>
+                        <Link :href="route('post.index', { slug: post.slug})">Show More</Link>
                     </div>
+                    <pagination class="mt-6" :links="posts.links" />
                 </div>
             </div>
         </div>
